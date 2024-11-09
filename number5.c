e8a88bcc0ca390af84199c318fb4e0610174f2f2
Satoshi Nemoto <s23c1104tl@s.chibakoudai.jp>
2024-11-09 21:45:48 +0900

    creat bun

--- number5.c
+++ number5.c
@@ -1,18 +1,58 @@
 #include<ctype.h>
 #include<stdio.h>
 #include<stdlib.h>
-#include <string.h>
+#include<string.h>
+#include<time.h>
 
-char moji[10000];
 
 int main(void){
-	FILE * fp = NULL;
-	fp = fopen("test.txt", "r");
-	int i;
-	while ((moji[i] = fgetc(fp)) != EOF){
-	i += 1;
+
+	srand((unsigned int)time(NULL));
+
+        FILE*fp;
+	char moji[10000];
+	char bun[10000];
+
+	fp = fopen("test.txt","r");
+        if(fp == NULL){
+		printf("%s file not open!\n","test.txt");
+		return -1;
 	}
+        int i;
+	while((moji[i] = fgetc(fp)) != EOF){
+	i++;
+	}
+	fclose(fp);
+        
+	printf("文の文字数は\n");
         int M = strlen(moji);
 	printf("%d\n",M);
+        
+        int j;
+	for(int i = 0;moji[i] != EOF; i++){
+           if(isalpha(moji[i])){
+             	   bun[j] = moji[i];
+	   j++;
+	   }else if(moji[i] == 20){
+		    bun[j] = moji[i];
+	   j++;
+	   }else if(moji[i] == 10){
+		    bun[j] = moji[i];
+           j++;
+	   }
+	}
+	printf("アルファベットのみの文の文字数は\n");
+	M = strlen(bun);
+	printf("%d\n",M);
+
+        int k; 
+        printf("文を生成\n");
+
+        for(int i = 0; i < 101; i++){
+           k = rand()%M;
+           putchar(bun[k]);
+	}
+
+        printf("\n");
 		return 0;
 }
